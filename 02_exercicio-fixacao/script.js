dbff8e81d7f49d697281ed6dd3c8241c6133a43c
Tiberio Ferreira <tiberio.ferreiracs@gmail.com>
2022-07-20 21:32:14 -0300

    Aula de operadores

--- 02_exercicio-fixacao/script.js
+++ 02_exercicio-fixacao/script.js
@@ -1,9 +1,16 @@
 // EXERCICIO 01
 const minhaIdade = prompt('Qual sua idade?')
 const idadeAmiga = prompt('Qual a idade da sua amiga ou amigo?')
+console.log("Sua idade é maior do que a do seu melhor amigo", minhaIdade > idadeAmiga);
+console.log(minhaIdade - idadeAmiga);
 
 // EXERCICIO 02
 const a = 'vou pra praia'
 const b = 'sou maior de idade'
 const c = 'posso tomar uma cervejinha'
 const d = 'quero passear de carro'
+
+!b && d; //Não sou maior de idade e quero passear de carro
+(b && a) && !d; //Sou maior de idade e vou para a praia, e não quero passear de carro
+a || d; //Vou para praia ou quero passear de carro
+c && !d; //Posso tomar uma cervejinha e não vou passear de carro
